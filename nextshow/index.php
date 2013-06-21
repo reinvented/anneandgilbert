988f099886fd87ba374935a6bc8f7e32cd00816a
Peter Rukavina <peter@rukavina.net>
2013-06-21 09:44:28 -0300

    Added "next show" app

---
+++ nextshow/index.php
@@ -0,0 +1,14 @@
+<?php
+include_once("../schedule/index.php");
+$next = nextAnneAndGilbert();
+?>
+<head>
+	<title>When is the next performance of Anne and Gilbert?</title>
+	<link type="text/css" rel="stylesheet" media="all" href="style.css" />
+</head>
+<body>
+	<h1>The next performance of Anne and Gilbert is in</h1>
+	<h2><?php print $next->hours_until; ?></h2>
+	<h3>(<a href="https://secure.ticketpro.ca/defn_7484911.html?lang=en#def_7484911">Buy Tickets Now</a>)</h3>
+	<h4>This is unofficial and may be wrong. <a href="https://github.com/reinvented/anneandgilbert">Source code here</a>.</h4>
+</body>
---
+++ schedule/nextanneandgilbert.php
@@ -0,0 +1,40 @@
+<?php
+
+function nextAnneAndGilbert() {
+	date_default_timezone_set("America/Halifax");
+	$schedule = file_get_contents("schedule.json");
+	$performances = json_decode($schedule);
+	$rightnow = mktime();
+
+	foreach($performances as $key => $performance) {
+		if ($performance->start >= $rightnow) {
+			$next['formatted_start'] = strftime("%A, %B, %e, %Y at %l:%M %p",$performance->start);
+			$next['formatted_end'] = strftime("%A, %B, %e, %Y at %l:%M %p",$performance->end);
+			$next['raw_start'] = $performance->start;
+			$next['raw_end'] = $performance->end;
+			$next['seconds_until'] = $performance->start - $rightnow;
+			$next['hours_until'] = time_until($next['seconds_until']);
+			$next['name'] = $performance->name;
+			break;
+		}
+	}
+	return $next;
+}
+
+function time_until($secs) {
+	$bit = array(
+		' year'        => $secs / 31556926 % 12,
+		' week'        => $secs / 604800 % 52,
+		' day'        => $secs / 86400 % 7,
+		' hour'        => $secs / 3600 % 24,
+		' minute'    => $secs / 60 % 60,
+		);
+   
+	foreach($bit as $k => $v){
+		if($v > 1)$ret[] = $v . $k . 's';
+		if($v == 1)$ret[] = $v . $k;
+		}
+	array_splice($ret, count($ret)-1, 0, 'and');
+
+	return join(' ', $ret);
+}
